e07b23b7d69dfed8d2f6aa74af1dbc201fed6d34
ClauCalv <claucalv@gmail.com>
2020-11-27 12:23:33 -0300

    Práticas 4 e 5 prontas
    Signed-off-by: ClauCalv <claucalv@gmail.com>

--- pratica4/codigos/run_barrier.bat
+++ pratica4/codigos/run_barrier.bat
@@ -1,4 +1,4 @@
-set ZK="C:\temp\zookeeper-3.4.14"
+set ZK="C:\Program Files\Zookeeper"
 set CP_ZK=.;%ZK%\zookeeper-3.4.14.jar;%ZK%\lib\slf4j-log4j12-1.7.25.jar;%ZK%\lib\slf4j-api-1.7.25.jar;%ZK%\lib\log4j-1.2.17.jar
 javac -cp %CP_ZK% *.java
 @echo ***** Barrier test
