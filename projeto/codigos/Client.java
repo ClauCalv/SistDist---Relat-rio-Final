69261b0b836428f0143bff1ba67a05d85ac2f90b
Arthur <arthur.h.f13@gmail.com>
2020-12-01 15:02:16 -0300

    criando node cliente

--- projeto/codigos/Client.java
+++ projeto/codigos/Client.java
@@ -1,28 +1,47 @@
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+
 public class Client {
 
     public static final int USER_NOT_FOUND = 0, LOGIN_VALID = 1, BAD_PASSWORD = 2;
-    private static String address = "localhost"; //default
+    private static String address = "localhost"; // default
 
     public static int queryLogin(String login, String senha) {
-        return 1; //TODO
+        return 1; // TODO
     }
 
     public static void setup(String address) {
-        if(address != null && !address.isEmpty())
+        if (address != null && !address.isEmpty())
             Client.address = address;
     }
 
     public static Client createUser(String login, String senha) {
-        //TODO
+        string root = login;
+        // Create ZK node name
+        if (zk != null) {
+            try {
+                Stat s = zk.exists(root, false);
+                if (s == null) {
+                    zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                }
+            } catch (KeeperException e) {
+                System.out.println("Keeper exception when instantiating queue: " + e.toString());
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted exception");
+            }
+        }
+
         return null;
     }
 
     public static Client doLogin(String login, String senha) {
-        //TODO
+        // TODO
         return null;
     }
 
     public void quit() {
-        //TODO
+        // TODO
     }
 }
